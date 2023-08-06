6301b69f869728c044b5aef7750667532a23af15
Chandni Kumari <108781610+chandnii96@users.noreply.github.com>
2023-08-07 00:13:05 +0530

    Update StringPermutations.java

--- Arrays/StringPermutations.java
+++ Arrays/StringPermutations.java
@@ -8,3 +8,54 @@ S = AAA
 Output: AAA AAA AAA AAA AAA AAA
 Explanation: There are total 6 permutations, as given in the output.
 */
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        while (T-- > 0) {
+            Solution ob = new Solution();
+            String S = sc.nextLine();
+            ArrayList<String> arr = ob.permutation(S);
+
+            for (String s : arr) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution
+{
+    ArrayList<String> res;
+    public ArrayList<String> permutation(String S) {
+        //Your code here
+      
+      res=new ArrayList<>();
+        fun(S.toCharArray(),new char[S.length()],0);
+        Collections.sort(res);
+        return res;
+    }
+    
+    public void fun(char[] s, char[] temp, int x){
+        if(x>=s.length){
+            res.add(new String(temp));
+            return;
+        }
+        for(int i=0;i<s.length;i++){
+            if(s[i]!='-'){
+                temp[x]=s[i];
+                char prev=s[i];
+                s[i]='-';
+                fun(s,temp,x+1);
+                s[i]=prev;
+            }
+        }
+    }  
+}
+
