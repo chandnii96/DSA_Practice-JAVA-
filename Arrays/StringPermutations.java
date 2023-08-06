8f1d4177545c0ed51b51aec29aad0a19ad8e8983
Chandni Kumari <108781610+chandnii96@users.noreply.github.com>
2023-08-06 23:56:45 +0530

    Create StringPermutations.java

---
+++ Arrays/StringPermutations.java
@@ -0,0 +1,10 @@
+/*
+Given a string S. The task is to find all permutations (need not be different) of a given string.
+
+Example 1:
+
+Input:
+S = AAA
+Output: AAA AAA AAA AAA AAA AAA
+Explanation: There are total 6 permutations, as given in the output.
+*/
