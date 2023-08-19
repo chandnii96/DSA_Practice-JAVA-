22dad16fa65d9645a4c1a3f955ef5b6354f3ed92
Chandni Kumari <108781610+chandnii96@users.noreply.github.com>
2023-08-19 22:33:06 +0530

    Update LeadersInArray.java

--- Arrays/LeadersInArray.java
+++ Arrays/LeadersInArray.java
@@ -14,3 +14,55 @@ leader is 5. The right most element
 is always a leader so it is also 
 included.
 */
+class Array {
+public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the test cases
+        while (t-- > 0) {
+            // Input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // Inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // Calling leaders() function
+            res = obj.leaders(arr, n);
+
+            // Printing the result
+            for (int i = 0; i < res.size(); i++) {
+                out.print(res.get(i) + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        int maxRight = arr[n - 1];
+        result.add(maxRight);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxRight) {
+                maxRight = arr[i];
+                result.add(maxRight);
+            }
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+}
