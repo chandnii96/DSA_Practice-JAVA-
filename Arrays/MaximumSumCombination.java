d38ade807115bd66863420fbf00eaf61bf354e23
Chandni Kumari <108781610+chandnii96@users.noreply.github.com>
2023-09-25 23:27:24 +0530

    Create MaximumSumCombination.java

---
+++ Arrays/MaximumSumCombination.java
@@ -0,0 +1,19 @@
+/*
+Given two integer array A and B of size N each.
+A sum combination is made by adding one element from array A and another element of array B.
+Return the maximum K valid sum combinations from all the possible sum combinations.
+
+Note : Output array must be sorted in non-increasing order.
+
+Example 1:
+
+Input:
+N = 2
+K = 2
+A [ ] = {3, 2}
+B [ ] = {1, 4}
+Output: {7, 6}
+Explanation: 
+7 -> (A : 3) + (B : 4)
+6 -> (A : 2) + (B : 4)
+*/
